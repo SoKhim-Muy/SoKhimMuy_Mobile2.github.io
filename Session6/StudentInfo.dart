1b250b68e6cbb1b5fbb33933bfa58998ecf86a08
SoKhim Muy <sokhimmuy181@gmail.com>
2026-07-07 07:32:48 +0700

    Add files via upload

---
+++ Session6/StudentInfo.dart
@@ -0,0 +1,12 @@
+class Student {
+  String name = " ";
+  int age = 0;
+}
+
+void main() {
+  Student std = Student();
+  std.name = "Khim";
+  std.age = 20;
+  print(std.name);
+  print(std.age);
+}
